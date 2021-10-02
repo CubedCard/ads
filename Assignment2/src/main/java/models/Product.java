c3385108f5a0028991e7015c80b94e43d513f39b
J. Derksen <jip.derksen@hva.nl>
2021-10-02 14:57:08 +0200

    added the functionality for the fromLine method in Product.java. As a result the ProductTest is green

--- Assignment2/src/main/java/models/Product.java
+++ Assignment2/src/main/java/models/Product.java
@@ -8,6 +8,7 @@ public class Product {
     public Product(long barcode) {
         this.barcode = barcode;
     }
+
     public Product(long barcode, String title, double price) {
         this(barcode);
         this.title = title;
@@ -16,14 +17,18 @@ public class Product {
 
     /**
      * parses product information from a textLine with format: barcode, title, price
+     *
      * @param textLine
-     * @return  a new Product instance with the provided information
-     *          or null if the textLine is corrupt or incomplete
+     * @return a new Product instance with the provided information
+     * or null if the textLine is corrupt or incomplete
      */
     public static Product fromLine(String textLine) {
-        Product newProduct = null;
+        Product newProduct;
+        String[] splitText;
 
-        // TODO convert the information in line to a new Product instance
+        splitText = textLine.split(", ");
+
+        newProduct = new Product(Long.parseLong(splitText[0]), splitText[1], Double.parseDouble(splitText[2]));
 
         return newProduct;
     }
@@ -48,8 +53,18 @@ public class Product {
     public boolean equals(Object other) {
         if (this == other) return true;
         if (!(other instanceof Product)) return false;
-        return this.getBarcode() == ((Product)other).getBarcode();
+        return this.getBarcode() == ((Product) other).getBarcode();
     }
 
     // TODO add public and private methods as per your requirements
+
+
+    @Override
+    public String toString() {
+        return String.format("%d/%s/%.2f",
+                getBarcode(),
+                getTitle(),
+                getPrice()
+        );
+    }
 }
