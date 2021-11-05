8792538362116ed7017b4da72d0a64805ec84c77
J. Derksen <jip.derksen@hva.nl>
2021-11-05 18:08:02 +0100

    added repeating and battery is running low

--- Assignment3/src/test/java/nl/hva/ict/ads/SortPerformanceTest.java
+++ Assignment3/src/test/java/nl/hva/ict/ads/SortPerformanceTest.java
@@ -2,7 +2,7 @@ package nl.hva.ict.ads;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -28,7 +28,25 @@ class SortPerformanceTest {
         System.out.println("hi");
     }
 
-    @Test
+    @RepeatedTest(10)
+    void collectionSortPerformanceTest() {
+        double time;
+        do {
+            this.doubleNumberOfArchers();
+            List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
+
+            System.gc();
+
+            long start = System.nanoTime();
+            archers.sort(scoringScheme);
+            long end = System.nanoTime();
+
+            time = ((end - start) / 1E6);
+            System.out.printf("Time taken (Collection sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
+        } while (numberOfArchers < 5E6 && time < 20000);
+    }
+
+    @RepeatedTest(10)
     void selInsSortPerformanceTest() {
         double time;
         do {
@@ -42,11 +60,12 @@ class SortPerformanceTest {
             long end = System.nanoTime();
 
             time = ((end - start) / 1E6);
-            System.out.printf("Time taken (selInsSort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
+            System.out.printf("Time taken (Selection / insertion sort): %.2f ms\nNumber Of archers: %d\n", time,
+                    numberOfArchers);
         } while (numberOfArchers < 5E6 && time < 20000);
     }
 
-    @Test
+    @RepeatedTest(10)
     void quickSortPerformanceTest() {
         double time;
         do {
@@ -60,25 +79,26 @@ class SortPerformanceTest {
             long end = System.nanoTime();
 
             time = ((end - start) / 1E6);
-            System.out.printf("Time taken (selInsSort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
+            System.out.printf("Time taken (Quick sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
         } while (numberOfArchers < 5E6 && time < 20000);
     }
 
-    @Test
+    @RepeatedTest(10)
     void topHeapSortPerformanceTest() {
         double time;
         do {
             this.doubleNumberOfArchers();
+            if (numberOfArchers > 5E6) numberOfArchers = (int) 5E6;
             List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
 
             System.gc();
 
             long start = System.nanoTime();
-            sorter.quickSort(archers1, scoringScheme);
+            sorter.topsHeapSort(5, archers1, scoringScheme);
             long end = System.nanoTime();
 
             time = ((end - start) / 1E6);
-            System.out.printf("Time taken (selInsSort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
+            System.out.printf("Time taken (Top 5 heap sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
         } while (numberOfArchers < 5E6 && time < 20000);
     }
 
