c65de6b2d79b1262585e3ab4c30a0513be6e3f87
J. Derksen <jip.derksen@hva.nl>
2021-11-06 12:30:16 +0100

    improved the test class to print results, repeat 10 times, less duplicate code and more

--- Assignment3/src/test/java/nl/hva/ict/ads/SortPerformanceTest.java
+++ Assignment3/src/test/java/nl/hva/ict/ads/SortPerformanceTest.java
@@ -1,5 +1,6 @@
 package nl.hva.ict.ads;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
@@ -9,49 +10,71 @@ import java.util.Comparator;
 import java.util.List;
 
 class SortPerformanceTest {
+    private final double MAX_NUMBER_OF_RECORDS = 5E6;
     private long seed = 1L;
     protected Sorter<Archer> sorter = new ArcherSorter();
     ChampionSelector championSelector = new ChampionSelector(seed);
-    protected List<Archer> archers = new ArrayList<>(championSelector.enrollArchers(5000000));
+    protected List<Archer> archers = new ArrayList<>(championSelector.enrollArchers((int) this.MAX_NUMBER_OF_RECORDS));
     protected Comparator<Archer> scoringScheme = Archer::compareByHighestTotalScoreWithLeastMissesAndLowestId;
     private int numberOfArchers;
+    private double time;
+
+    private static final List<Double> collectionSortTimes = new ArrayList<>();
+    private static final List<Double> selInsSortTimes = new ArrayList<>();
+    private static final List<Double> quickSortTimes = new ArrayList<>();
+    private static final List<Double> topHeapSortTimes = new ArrayList<>();
 
     @BeforeEach
     void setup() {
         System.gc();
-        seed++;
+        this.seed++;
         this.numberOfArchers = 100;
+        this.time = 0;
     }
 
     @AfterEach
     void printResults() {
-        System.out.println("hi");
+        System.out.println();
+    }
+
+    @AfterAll
+    static void print() {
+        for (int i = 0; i < Math.max((Math.max(collectionSortTimes.size(), selInsSortTimes.size())),
+                (Math.max(quickSortTimes.size(), topHeapSortTimes.size()))); i++) {
+            if (i < collectionSortTimes.size()) System.out.printf("%.2f;", collectionSortTimes.get(i));
+            else System.out.print(";");
+            if (i < selInsSortTimes.size()) System.out.printf("%.2f;", selInsSortTimes.get(i));
+            else System.out.print(";");
+            if (i < quickSortTimes.size()) System.out.printf("%.2f;", quickSortTimes.get(i));
+            else System.out.print(";");
+            if (i < topHeapSortTimes.size()) System.out.printf("%.2f;", topHeapSortTimes.get(i));
+            else System.out.print(";");
+            System.out.println();
+        }
     }
 
     @RepeatedTest(10)
     void collectionSortPerformanceTest() {
-        double time;
         do {
             this.doubleNumberOfArchers();
-            List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
+            List<Archer> archers1 = new ArrayList<>(archers.subList(0, numberOfArchers));
 
             System.gc();
 
             long start = System.nanoTime();
-            archers.sort(scoringScheme);
+            archers1.sort(scoringScheme);
             long end = System.nanoTime();
 
-            time = ((end - start) / 1E6);
-            System.out.printf("Time taken (Collection sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
-        } while (numberOfArchers < 5E6 && time < 20000);
+            this.calculateTime(start, end);
+            collectionSortTimes.add(this.time);
+        } while (condition());
     }
 
     @RepeatedTest(10)
     void selInsSortPerformanceTest() {
-        double time;
         do {
             this.doubleNumberOfArchers();
-            List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
+            List<Archer> archers1 = new ArrayList<>(archers.subList(0, numberOfArchers));
 
             System.gc();
 
@@ -59,18 +82,16 @@ class SortPerformanceTest {
             sorter.selInsSort(archers1, scoringScheme);
             long end = System.nanoTime();
 
-            time = ((end - start) / 1E6);
-            System.out.printf("Time taken (Selection / insertion sort): %.2f ms\nNumber Of archers: %d\n", time,
-                    numberOfArchers);
-        } while (numberOfArchers < 5E6 && time < 20000);
+            this.calculateTime(start, end);
+            selInsSortTimes.add(this.time);
+        } while (condition());
     }
 
     @RepeatedTest(10)
     void quickSortPerformanceTest() {
-        double time;
         do {
             this.doubleNumberOfArchers();
-            List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
+            List<Archer> archers1 = new ArrayList<>(archers.subList(0, numberOfArchers));
 
             System.gc();
 
@@ -78,32 +99,41 @@ class SortPerformanceTest {
             sorter.quickSort(archers1, scoringScheme);
             long end = System.nanoTime();
 
-            time = ((end - start) / 1E6);
-            System.out.printf("Time taken (Quick sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
-        } while (numberOfArchers < 5E6 && time < 20000);
+            this.calculateTime(start, end);
+            quickSortTimes.add(this.time);
+        } while (condition());
     }
 
     @RepeatedTest(10)
     void topHeapSortPerformanceTest() {
-        double time;
         do {
             this.doubleNumberOfArchers();
-            if (numberOfArchers > 5E6) numberOfArchers = (int) 5E6;
-            List<Archer> archers1 = new ArrayList<>(archers.subList(0,numberOfArchers));
+            List<Archer> archers1 = new ArrayList<>(archers.subList(0, numberOfArchers));
 
             System.gc();
 
             long start = System.nanoTime();
-            sorter.topsHeapSort(5, archers1, scoringScheme);
+            sorter.topsHeapSort(numberOfArchers, archers1, scoringScheme);
             long end = System.nanoTime();
 
-            time = ((end - start) / 1E6);
-            System.out.printf("Time taken (Top 5 heap sort): %.2f ms\nNumber Of archers: %d\n", time, numberOfArchers);
-        } while (numberOfArchers < 5E6 && time < 20000);
+            this.calculateTime(start, end);
+            topHeapSortTimes.add(this.time);
+        } while (condition());
+    }
+
+    private boolean condition() {
+        double MAX_TIME = 20000;
+        return numberOfArchers < this.MAX_NUMBER_OF_RECORDS && this.time < MAX_TIME;
     }
 
     private void doubleNumberOfArchers() {
         this.numberOfArchers = this.numberOfArchers * 2;
+        if (numberOfArchers > this.MAX_NUMBER_OF_RECORDS) numberOfArchers = (int) this.MAX_NUMBER_OF_RECORDS;
+    }
+
+    private void calculateTime(long start, long end) {
+        this.time = (end - start) / 1E6;
+        System.out.printf("Time taken: %.2f ms\nNumber Of archers: %d\n", this.time, numberOfArchers);
     }
 
 }
