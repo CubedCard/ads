d999795aacfed53eee4a33df9f42b9323a27481a
J. Derksen <jip.derksen@hva.nl>
2021-12-27 16:40:50 +0100

    implemented a lot of methods and improved some as well. Some methods still need improvement

--- Assignment5/src/main/java/graphs/DirectedGraph.java
+++ Assignment5/src/main/java/graphs/DirectedGraph.java
@@ -6,19 +6,21 @@ import java.util.stream.Collectors;
 
 public class DirectedGraph<V extends Identifiable, E> {
 
-    private final Map<String,V> vertices = new HashMap<>();
-    private final Map<V,Map<V,E>> edges = new HashMap<>();
+    private final Map<String, V> vertices = new HashMap<>();
+    private final Map<V, Map<V, E>> edges = new HashMap<>();
 
-    /** representation invariants:
-        1.  the vertices map stores all vertices by their identifying id (which prevents duplicates)
-        2.  the edges map stores all directed outgoing edges by their from-vertex and then in the nested map by their to-vertex
-        3.  there can only be two directed edges between any two given vertices v1 and v2:
-            one from v1 to v2 in edges.get(v1).get(v2)
-            one from v2 to v1 in edges.get(v2).get(v1)
-        4.  every vertex instance in the key-sets of edges shall also occur in the vertices map and visa versa
+    /**
+     * representation invariants:
+     * 1.  the vertices map stores all vertices by their identifying id (which prevents duplicates)
+     * 2.  the edges map stores all directed outgoing edges by their from-vertex and then in the nested map by their to-vertex
+     * 3.  there can only be two directed edges between any two given vertices v1 and v2:
+     * one from v1 to v2 in edges.get(v1).get(v2)
+     * one from v2 to v1 in edges.get(v2).get(v1)
+     * 4.  every vertex instance in the key-sets of edges shall also occur in the vertices map and visa versa
      **/
 
-    public DirectedGraph() { }
+    public DirectedGraph() {
+    }
 
     public Collection<V> getVertices() {
         return vertices.values();
@@ -26,9 +28,10 @@ public class DirectedGraph<V extends Identifiable, E> {
 
     /**
      * finds the vertex in the graph identified by the given id
+     *
      * @param id
-     * @return  the vertex that matches the given id
-     *          null if none of the vertices matches the id
+     * @return the vertex that matches the given id
+     * null if none of the vertices matches the id
      */
     public V getVertexById(String id) {
         return vertices.get(id);
@@ -37,17 +40,19 @@ public class DirectedGraph<V extends Identifiable, E> {
     /**
      * retrieves the collection of neighbour vertices that can be reached directly
      * via an out-going directed edge from 'fromVertex'
+     *
      * @param fromVertex
-     * @return  null if fromVertex cannot be found in the graph
-     *          an empty collection if fromVertex has no neighbours
+     * @return null if fromVertex cannot be found in the graph
+     * an empty collection if fromVertex has no neighbours
      */
     public Collection<V> getNeighbours(V fromVertex) {
-        if (fromVertex == null) return null;
+        if (fromVertex == null || !this.edges.containsKey(fromVertex)) return null;
 
         // TODO retrieve the collection of neighbour vertices of fromVertex out of the edges data structure
 
-        return null;
+        return this.edges.get(fromVertex).keySet();
     }
+
     public Collection<V> getNeighbours(String fromVertexId) {
         return this.getNeighbours(this.getVertexById(fromVertexId));
     }
@@ -56,17 +61,22 @@ public class DirectedGraph<V extends Identifiable, E> {
      * retrieves the collection of edges
      * which connects the 'fromVertex' with its neighbours
      * (only the out-going edges directed from 'fromVertex' towards a neighbour shall be included
+     *
      * @param fromVertex
-     * @return  null if fromVertex cannot be found in the graph
-     *          an empty collection if fromVertex has no out-going edges
+     * @return null if fromVertex cannot be found in the graph
+     * an empty collection if fromVertex has no out-going edges
      */
     public Collection<E> getEdges(V fromVertex) {
-        if (fromVertex == null) return null;
+//        if (fromVertex == null || !this.edges.containsKey(fromVertex)) return null;
+        if (!this.vertices.containsValue(fromVertex)) return null;
 
         // TODO retrieve the collection of out-going edges which connect fromVertex with a neighbour in the edges data structure
 
-        return null;
+        if (this.edges.containsKey(fromVertex)) return this.edges.get(fromVertex).values();
+
+        return List.of();
     }
+
     public Collection<E> getEdges(String fromId) {
         return this.getEdges(this.getVertexById(fromId));
     }
@@ -74,61 +84,72 @@ public class DirectedGraph<V extends Identifiable, E> {
     /**
      * Adds newVertex to the graph, if not yet present and in a way that maintains the representation invariants.
      * If a duplicate of newVertex (with the same id) already exists in the graph,
-     *      nothing will be added, and the existing duplicate will be kept and returned.
-     * @param newVertex
-     * @return  the duplicate of newVertex with the same id that already exists in the graph,
-     *          or newVertex itself if it has been added.
+     * nothing will be added, and the existing duplicate will be kept and returned.
+     *
+     * @param newVertex the vertex to be added if it's id is unique
+     * @return the duplicate of newVertex with the same id that already exists in the graph,
+     * or newVertex itself if it has been added.
      */
     public V addOrGetVertex(V newVertex) {
         // TODO add and return the newVertex, or return the existing duplicate vertex with the same Id
         //  pay attention to sustaining representation invariant items 1. and 4.
 
+        V currentVertex = this.vertices.putIfAbsent(newVertex.getId(), newVertex);
+
+        if (currentVertex == null) {
+            this.edges.put(newVertex, new HashMap<>());
+            return newVertex;
+        }
 
         // a proper vertex shall be returned at all times
-        return null;
+        return currentVertex;
     }
-
-
 
 
     /**
      * Adds a new, directed edge 'newEdge'
      * from vertex 'fromVertex' to vertex 'toVertex'
      * No change shall be made if a directed edge already exists between these vertices
-     * @param fromVertex    the start vertex of the directed edge
-     * @param toVertex      the target vertex of the directed edge
-     * @param newEdge       the instance with edge information
-     * @return  whether the edge has been added successfully
+     *
+     * @param fromVertex the start vertex of the directed edge
+     * @param toVertex   the target vertex of the directed edge
+     * @param newEdge    the instance with edge information
+     * @return whether the edge has been added successfully
      */
     public boolean addEdge(V fromVertex, V toVertex, E newEdge) {
         // TODO add (directed) newEdge to the graph between fromVertex and toVertex
+        if (fromVertex == null || toVertex == null) return false;
+        fromVertex = this.addOrGetVertex(fromVertex);
+        toVertex = this.addOrGetVertex(toVertex);
+        if (this.edges.get(fromVertex).containsKey(toVertex)) return false;
 
-        return false;
+        this.edges.get(fromVertex).put(toVertex, newEdge);
+        return true;
     }
 
     /**
      * Adds a new, directed edge 'newEdge'
      * from vertex with id=fromId to vertex with id=toId
      * No change shall be made if a directed edge already exists between these vertices
-     * @param fromId    the id of the start vertex of the outgoing edge
-     * @param toId      the id of the target vertex of the directed edge
-     * @param newEdge   the instance with edge information
-     * @return  whether the edge has been added successfully
+     *
+     * @param fromId  the id of the start vertex of the outgoing edge
+     * @param toId    the id of the target vertex of the directed edge
+     * @param newEdge the instance with edge information
+     * @return whether the edge has been added successfully
      */
     public boolean addEdge(String fromId, String toId, E newEdge) {
         // TODO add (directed) newEdge to the graph between fromId and toId
-
-
-        return false;
+        return this.addEdge(this.getVertexById(fromId), this.getVertexById(toId), newEdge);
     }
 
     /**
      * Adds two directed edges: one from v1 to v2 and one from v2 to v1
      * both with the same edge information
+     *
      * @param v1
      * @param v2
      * @param newEdge
-     * @return  whether both edges have been added
+     * @return whether both edges have been added
      */
     public boolean addConnection(V v1, V v2, E newEdge) {
         return this.addEdge(v1, v2, newEdge) && this.addEdge(v2, v1, newEdge);
@@ -137,10 +158,11 @@ public class DirectedGraph<V extends Identifiable, E> {
     /**
      * Adds two directed edges: one from id1 to id2 and one from id2 to id1
      * both with the same edge information
+     *
      * @param id1
      * @param id2
      * @param newEdge
-     * @return  whether both edges have been added
+     * @return whether both edges have been added
      */
     public boolean addConnection(String id1, String id2, E newEdge) {
         return this.addEdge(id1, id2, newEdge) && this.addEdge(id2, id1, newEdge);
@@ -148,25 +170,21 @@ public class DirectedGraph<V extends Identifiable, E> {
 
     /**
      * retrieves the directed edge between 'fromVertex' and 'toVertex' from the graph, if any
-     * @param fromVertex    the start vertex of the designated edge
-     * @param toVertex      the end vertex of the designated edge
-     * @return      the designated directed edge that has been registered in the graph
-     *              returns null if no connection has been set up between these vertices in the specified direction
+     *
+     * @param fromVertex the start vertex of the designated edge
+     * @param toVertex   the end vertex of the designated edge
+     * @return the designated directed edge that has been registered in the graph
+     * returns null if no connection has been set up between these vertices in the specified direction
      */
     public E getEdge(V fromVertex, V toVertex) {
         if (fromVertex == null || toVertex == null) return null;
         // TODO retrieve the directed edge between vertices fromVertex and toVertex from the graph
 
         Map<V, E> fromVertexMap = this.edges.getOrDefault(fromVertex, null);
-        Map<V, E> toVertexMap = this.edges.getOrDefault(toVertex, null);
 
-        if (fromVertexMap == null || toVertexMap == null) return null;
+        if (fromVertexMap == null) return null;
 
-        for (E edge : fromVertexMap.values()) {
-            if (toVertexMap.containsValue(edge)) return edge;
-        }
-
-        return null;
+        return fromVertexMap.get(toVertex);
     }
 
     public E getEdge(String fromId, String toId) {
@@ -174,7 +192,7 @@ public class DirectedGraph<V extends Identifiable, E> {
     }
 
     /**
-     * @return  the total number of vertices in the graph
+     * @return the total number of vertices in the graph
      */
     public int getNumVertices() {
         return vertices.size();
@@ -182,15 +200,15 @@ public class DirectedGraph<V extends Identifiable, E> {
 
     /**
      * calculates and returns the total number of directed edges in the graph data structure
-     * @return  the total number of edges in the graph
+     *
+     * @return the total number of edges in the graph
      */
     public int getNumEdges() {
         // TODO calculate and return the total number of directed edges in the graph
-        return (int) this.edges.values()
+        return this.edges.values()
                 .stream()
-                .map(Map::values)
-                .distinct()
-                .count();
+                .mapToInt(Map::size)
+                .sum();
     }
 
     /**
@@ -212,7 +230,7 @@ public class DirectedGraph<V extends Identifiable, E> {
         /**
          * representation invariants:
          * 1. vertices contains a sequence of vertices that are connected in the graph by a directed edge,
-         *    i.e. FOR ALL i: 0 < i < vertices.length: this.getEdge(vertices[i-1],vertices[i]) will provide edge information of the connection
+         * i.e. FOR ALL i: 0 < i < vertices.length: this.getEdge(vertices[i-1],vertices[i]) will provide edge information of the connection
          * 2. a path with one vertex has no edges
          * 3. a path without vertices is empty
          * totalWeight is a helper attribute to capture additional info from searches, not a fundamental property of a path
@@ -241,17 +259,20 @@ public class DirectedGraph<V extends Identifiable, E> {
             return this.totalWeight;
         }
 
-        public Set<V> getVisited() { return this.visited; }
+        public Set<V> getVisited() {
+            return this.visited;
+        }
     }
 
     /**
      * Uses a depth-first search algorithm to find a path from the start vertex to the target vertex in the graph
      * All vertices that are being visited by the search should also be registered in path.visited
+     *
      * @param startId
      * @param targetId
-     * @return  the path from start to target
-     *          returns null if either start or target cannot be matched with a vertex in the graph
-     *                          or no path can be found from start to target
+     * @return the path from start to target
+     * returns null if either start or target cannot be matched with a vertex in the graph
+     * or no path can be found from start to target
      */
     public DGPath depthFirstSearch(String startId, String targetId) {
 
@@ -270,11 +291,12 @@ public class DirectedGraph<V extends Identifiable, E> {
     /**
      * Uses a breadth-first search algorithm to find a path from the start vertex to the target vertex in the graph
      * All vertices that are being visited by the search should also be registered in path.visited
+     *
      * @param startId
      * @param targetId
-     * @return  the path from start to target
-     *          returns null if either start or target cannot be matched with a vertex in the graph
-     *                          or no path can be found from start to target
+     * @return the path from start to target
+     * returns null if either start or target cannot be matched with a vertex in the graph
+     * or no path can be found from start to target
      */
     public DGPath breadthFirstSearch(String startId, String targetId) {
 
@@ -320,15 +342,16 @@ public class DirectedGraph<V extends Identifiable, E> {
     /**
      * Calculates the edge-weighted shortest path from start to target
      * according to Dijkstra's algorithm of a minimum spanning tree
-     * @param startId       id of the start vertex of the search
-     * @param targetId      id of the target vertex of the search
-     * @param weightMapper    provides a function, by which the weight of an edge can be retrieved or calculated
-     * @return  the shortest path from start to target
-     *          returns null if either start or target cannot be matched with a vertex in the graph
-     *                          or no path can be found from start to target
+     *
+     * @param startId      id of the start vertex of the search
+     * @param targetId     id of the target vertex of the search
+     * @param weightMapper provides a function, by which the weight of an edge can be retrieved or calculated
+     * @return the shortest path from start to target
+     * returns null if either start or target cannot be matched with a vertex in the graph
+     * or no path can be found from start to target
      */
     public DGPath dijkstraShortestPath(String startId, String targetId,
-                                       Function<E,Double> weightMapper) {
+                                       Function<E, Double> weightMapper) {
 
         V start = getVertexById(startId);
         V target = getVertexById(targetId);
@@ -361,10 +384,6 @@ public class DirectedGraph<V extends Identifiable, E> {
             //  if you hit the target: complete the path and bail out !!!
 
 
-
-
-
-
             // TODO find the next nearest node that is not marked yet
 
         }
@@ -380,8 +399,8 @@ public class DirectedGraph<V extends Identifiable, E> {
                 .map(v -> v.toString() + ": " +
                         this.edges.get(v).entrySet().stream()
                                 .map(e -> e.getKey().toString() + "(" + e.getValue().toString() + ")")
-                                .collect(Collectors.joining(",","[","]"))
-                        )
-                .collect(Collectors.joining(",\n  ","{ ","\n}"));
+                                .collect(Collectors.joining(",", "[", "]"))
+                )
+                .collect(Collectors.joining(",\n  ", "{ ", "\n}"));
     }
 }
